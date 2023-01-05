781202bf5f0922e08d8941d7256f3e05f5d995d8
Hyerin <hyerin.bcit@gmail.com>
2023-01-05 01:23:50 -0800

    Added dark mode

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 // fetch the items from JSON file
-function loadData(){
-  return fetch('data/data.json')
-  .then(response => response.json())
-  .then(json => json.items);
+function loadData() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
 }
 
 // update the list with the given items
-function displayItems(data){
-  const dataContainer = document.querySelector('.data');
-  dataContainer.innerHTML = data.map(list => createHTMLString(list)).join('');
-
+function displayItems(data) {
+  const dataContainer = document.querySelector(".data");
+  dataContainer.innerHTML = data.map((list) => createHTMLString(list)).join("");
 }
 
-// create HTML list item from the given data 
-function createHTMLString(list){
+// create HTML list item from the given data
+function createHTMLString(list) {
   return `
     <li class="list">
       <img src="${list.image}" alt="${list.type}" class="list-thumnail"/>
@@ -22,7 +21,7 @@ function createHTMLString(list){
   `;
 }
 
-function onBttnClick(event, data){
+function onBttnClick(event, data) {
   // console.log("key is " + event.target.dataset.key)
   // console.log("value is " + event.target.dataset.value)
 
@@ -30,26 +29,46 @@ function onBttnClick(event, data){
   const key = dataset.key;
   const value = dataset.value;
 
-  if(key == null || value == null){
+  if (key == null || value == null) {
     return;
   }
 
-  const filtered = data.filter(list => list[key] === value);
-  console.log(filtered)
+  const filtered = data.filter((list) => list[key] === value);
+  console.log(filtered);
   displayItems(filtered);
 }
 
-function setEventListeners(data){
-  const logo = document.querySelector('.logo');
-  const buttons = document.querySelector('.bodyContent');
-  logo.addEventListener('click', () => displayItems(data));
-  buttons.addEventListener('click', event => onBttnClick(event, data));
+function setEventListeners(data) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".bodyContent");
+  logo.addEventListener("click", () => displayItems(data));
+  buttons.addEventListener("click", (event) => onBttnClick(event, data));
 }
 
+function chngimg(){
+  var toggleImg = document.querySelector(".switchMode").src
+  if (toggleImg.indexOf('Moon.png') != -1) {
+    document.querySelector(".switchMode").src = "img/Sun.png";
+  } else {
+    document.querySelector(".switchMode").src = "img/Moon.png";
+  }
+}
+
+function darkMode() {
+  var background = document.body;
+  background.classList.toggle("dark-mode");
+
+  const toggleButton = document.querySelector(".toggle");
+  toggleButton.classList.toggle("toogle-darkMode");
+
+  document.querySelector(".blue").classList.toggle("blue-darkMode");
+  document.querySelector(".yellow").classList.toggle("yellow-darkMode");
+  document.querySelector(".pink").classList.toggle("pink-darkMode");
+}
 // main
 loadData()
-.then(data => {
-  displayItems(data);
-  setEventListeners(data);
-})
-.catch(console.log);
+  .then((data) => {
+    displayItems(data);
+    setEventListeners(data);
+  })
+  .catch(console.log);
